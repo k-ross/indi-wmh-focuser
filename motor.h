d7a174b5bb14afb28a03e36cf63f384db641144d
Kevin Ross <kevin@familyross.net>
2021-08-01 22:02:11 -0400

    Source code cleanup

--- motor.h
+++ motor.h
@@ -4,41 +4,41 @@
 
 enum class BoardRevision
 {
-	Original, Rev21
+    Original, Rev21
 };
 
 class Motor
 {
 protected:
-	BoardRevision _revision = BoardRevision::Original;
-	
-	void _Delay(int microseconds)
-	{
-		if (microseconds == 0)
-			return;
+    BoardRevision _revision = BoardRevision::Original;
+    
+    void _Delay(int microseconds)
+    {
+        if (microseconds == 0)
+            return;
 
-		auto start = std::chrono::high_resolution_clock::now();
-		for(;;)
-		{
-			auto later = std::chrono::high_resolution_clock::now();
-			auto micros = std::chrono::duration_cast<std::chrono::microseconds>(later - start);
-			if (micros.count() >= microseconds)
-				break;
-		}
-	}
-	
+        auto start = std::chrono::high_resolution_clock::now();
+        for(;;)
+        {
+            auto later = std::chrono::high_resolution_clock::now();
+            auto micros = std::chrono::duration_cast<std::chrono::microseconds>(later - start);
+            if (micros.count() >= microseconds)
+                break;
+        }
+    }
+    
 public:
-	enum class Direction
-	{
-		Forward, Backward
-	};
-	
-	Motor() {}
-	virtual ~Motor() {}
-	
-	void SetBoardRevision(BoardRevision revision) { _revision = revision; }
-	
-	virtual void Enable(Direction dir) = 0;
-	virtual void Disable() = 0;
-	virtual void SingleStep(int stepDelayMicroseconds) = 0;
+    enum class Direction
+    {
+        Forward, Backward
+    };
+    
+    Motor() {}
+    virtual ~Motor() {}
+    
+    void SetBoardRevision(BoardRevision revision) { _revision = revision; }
+    
+    virtual void Enable(Direction dir) = 0;
+    virtual void Disable() = 0;
+    virtual void SingleStep(int stepDelayMicroseconds) = 0;
 };
